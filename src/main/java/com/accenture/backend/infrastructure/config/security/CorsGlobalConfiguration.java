695a126b618c3130f636baadb784999a5cacf9e0
Santiago Tabares <tabaress@globalhitss.com>
2025-07-11 00:08:58 -0500

    chore: configure project for cloud deployment

--- src/main/java/com/accenture/backend/infrastructure/config/security/CorsGlobalConfiguration.java
+++ src/main/java/com/accenture/backend/infrastructure/config/security/CorsGlobalConfiguration.java
@@ -14,7 +14,7 @@ public class CorsGlobalConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("*")
+                        .allowedOrigins("*")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
